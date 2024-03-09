82ee6077a97248d2538a18ca340ea73cc086c0f7
DenisQbit <billchifer@bk.ru>
2024-03-10 00:12:33 +0700

    Создаём функция, которая принимает переменное количество аргументов
    В будущем будем реализовывать что-то типо библиотеки функций для расширения функционала языка С++. 09.03.2024 20:11 по МСК скачался WarThunder. Пойду гамать ))

---
+++ NthArgumentExtractor/NthArgumentExtractor/NthArgumentExtractor.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+#include <tuple>
+
+
+template<typename... Args>
+void variadic_function(Args... args) {
+    // Создаем кортеж из аргументов
+    auto t = std::make_tuple(args...);
+    // Печатаем кортеж
+    std::apply([](auto&&... args) { ((std::cout << args << ' '), ...); std::cout << '\n'; }, t);
+}
+
+
+
+int main() {
+
+    variadic_function(1, 2.5, "Hello");
+    return 0;
+}
