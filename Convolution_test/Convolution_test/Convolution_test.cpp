b5b765519f14a9730b27ad0d10cd490f1638cfb0
DenisQbit <billchifer@bk.ru>
2024-03-11 23:01:21 +0700

    Work machine - Server
    Для получения на локальную машину. Надо убудет написать скрипт автоматического апдейта файла по указанному времени.

--- Convolution_test/Convolution_test/Convolution_test.cpp
+++ Convolution_test/Convolution_test/Convolution_test.cpp
@@ -41,15 +41,56 @@ static int arc_test(int arr[]) {
 
 int main()
 {
-    int conv_1[] = {
-    1, 2, 3, /*4, 5, 6, 7, 8, 9,*/
+    int conv_1[] = { 1, 2, 3, /*4, 5, 6, 7, 8, 9,*/ };
+    int conv_test[] = {1, 2, 3,/*4*/};
+    
+    int matrix[] = {
+            4,  5,  6,
+
+        1,  4,   5,  6,
+
+        2,  8,  10,  12,
+
+        3,  12, 15,  18
+
     };
+
+    int couting[] =
+    {
+    conv_1[0] * conv_test[0]
+    };
+
+
+
+
+
     int conv_2[][3] = {
         {1, 2, 3},
         {4, 5, 6},
         //{7, 8, 9}
     };
     auto m = "d";
+
+    int conv_cout[3] = {};
+    //создаём нормальную сборку ))    //свёртку*
+    size_t size_conv_1 = size(conv_1);
+    //cout << size_conv_1 << endl;
+    size_t size_conv_2 = size(conv_test);
+    //cout << size_conv_2 << endl;
+    int parametr = size_conv_1 + size_conv_2 - 1;
+
+    for (int i = 1; i <= size(conv_1) + size(conv_2) - 1 /*6*/; i++) {
+        break;
+    }
+
+    for (size_t i = 1; i <= size_conv_1; i++)
+    {
+        for (size_t i = 1; i <= size_conv_2; i++)
+        {
+            int a = 0;
+        }
+    }
+
     int conv_test[] = { 1, 2, 3, 4 };
     int conv_cout[3] = {};
 
@@ -58,9 +99,8 @@ int main()
     int a = 0;
     for (size_t i = 0; i < size(conv_test); i++)
     {
-        //cout << ++a;
+        cout << ++a;
     }
-
     cout << arc_test(conv_1);
 
     //level_2(conv_2, 2, 2);
