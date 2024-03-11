1f7ac8d5d78557a819c16af32d69f56faaaaaae2
DenisQbit <billchifer@bk.ru>
2024-03-11 22:47:09 +0700

    Update ( --pull )

--- Convolution_test/Convolution_test/Convolution_test.cpp
+++ Convolution_test/Convolution_test/Convolution_test.cpp
@@ -2,20 +2,42 @@
 #include <iostream>
 #include <string>
 #include <type_traits> // Для std::extent
+
 using namespace std;
 
-/*
+template <typename T, size_t Ta >
+int level_1(T(&arr1)[Ta], int f) {
 
-*/
+    for (size_t i = 0; i < f; i++)
+    {
+            return arr1[i];
+    }
+}
+
+template <typename T,size_t Ta, size_t Taa >
+void level_2(T(&arr1)[Ta][Taa], int f, int f2) {
+
+    for (size_t i = 0; i < f; i++)
+    {
+        for (size_t j = 0; j < f2; j++)
+        {
+           cout << arr1[i][j];
+        }
+    }
+}
+
 void arc(int arr[]) {
     // cout << extent<decltype(arr), -1>::value;
     for (int i = 0; i < extent<decltype(arr), 0>::value; i++) {
         cout << extent<decltype(arr), 0>::value << endl;
     }
-
 }
 
-void arc_test(int arr[]) { } 
+int size(int arr[]) { return sizeof(arr) / sizeof(arr[0]) + 1; }
+
+static int arc_test(int arr[]) {
+        return size(arr);
+}
 
 int main()
 {
@@ -28,37 +50,20 @@ int main()
         //{7, 8, 9}
     };
     auto m = "d";
-    int conv_test[] = {1, 2, 3,4};
+    int conv_test[] = { 1, 2, 3, 4 };
     int conv_cout[3] = {};
 
-    
     int sum = 0;
-    for (int i = 0; i < extent<decltype(conv_test), 0>::value; i++) {
-        for (int j = 0; j < extent<decltype(conv_1), 0>::value; j++) {
-            sum += conv_test[i] * conv_1[j];
-            cout << sum<< " ";
-        }
-        cout << endl;
+    
+    int a = 0;
+    for (size_t i = 0; i < size(conv_test); i++)
+    {
+        //cout << ++a;
     }
 
+    cout << arc_test(conv_1);
+
+    //level_2(conv_2, 2, 2);
     //arc(conv_1);
-
-        for (int i = 0; i < extent<decltype(conv_2), 0>::value; i++) {
-        //cout << "i = " << i << " ";
-        int sum = 0;
-        for (int j = 0; j < extent<decltype(conv_2), 1>::value; j++)
-        {
-            cout << "i = " << i << " ";
-            //cout << "j = " << j << " " << endl;
-            cout << "j = " << j << " ";
-            //cout << i << " " << j << " = ";
-            sum = conv_2[i][j] * conv_test[j];
-            cout << conv_2[i][j] << " ";
-            cout << sum << " " << endl;
-            //cout << sum << endl;
-        }
-        //cout << sum << endl;
-    }
-    // std::cout << "Hello World!\n";
-}
-
+    
+};
