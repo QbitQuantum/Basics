0bdc59d54270d3b8c5da7daa8cd5701fe3def41e
DenisQbit <billchifer@bk.ru>
2024-03-25 10:24:02 +0700

    Класс, который проверяет на: доступ к файлу, допустимые имена файлов, наличие проверки на имя файла

---
+++ UsingMacros/UsingMacros/ConfigFile/Config.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+class CAdmin {
+public:
+	//bool
+	bool access_permission = true;
+	bool check_FilesName = false;
+	bool source_code = true;
+	//std::string
+	std::string FilesName = "UsingMacros.cpp";
+
+	//int
+
+};
---
+++ UsingMacros/UsingMacros/UsingMacros.cpp
@@ -0,0 +1,74 @@
+﻿// Library
+#include <iostream>
+#include <fstream>
+#include <string>
+
+// Files
+#include "ConfigFile/Config.cpp"
+
+//Function
+int check_filesname(std::string filename, std::string admin_filename) {
+
+    return (filename.find(admin_filename) != std::string::npos) ? 1 : 0;
+}
+
+int main() {
+    
+    CAdmin obj;
+
+    if (obj.access_permission) {
+        std::string filename;
+        int lineNumber;
+        std::cout << "Input name file: ";
+        std::cin >> filename;
+        if (obj.check_FilesName) {
+            while (check_filesname(filename, obj.FilesName))
+            {
+            std::cout << "Error: You cannot use ";
+            std::cout << filename;
+            std::cout << "\n";
+            std::cout << "Input name file: ";
+            std::cin >> filename;
+            }
+        }
+        if (obj.source_code) {
+            std::ifstream file(filename);
+
+            if (!file.is_open()) {
+                std::cerr << "Не удалось открыть файл!" << std::endl;
+                return 1;
+            }
+            std::string line;
+            while (std::getline(file, line)) { 
+                std::cout << line << std::endl; 
+            }
+            file.close(); 
+            return 0;
+        }
+        else {
+        std::cout << "Input number line: ";
+        std::cin >> lineNumber;
+
+        std::ifstream file(filename);
+        if (!file.is_open()) {
+            std::cout << "Error open file " << filename << std::endl;
+            return 1;
+        }
+        std::string line;
+        int currentLine = 0;
+        while (std::getline(file, line)) {
+            currentLine++;
+            if (currentLine == lineNumber) {
+                std::cout << "Line number " << lineNumber << ": " << line << std::endl;
+                break;
+            }
+        }
+        if (currentLine < lineNumber) {
+            std::cout << "Mnogo vvel " << lineNumber << std::endl;
+        }
+        file.close();
+        return 0;
+        }
+        
+    }
+}
