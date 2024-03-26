5abd01b10be967cb2d209618e5762cbb2c8021db
DenisQbit <billchifer@bk.ru>
2024-03-26 14:06:42 +0700

    Попытка вернуть значение функции через переданный тип переменной (False)

---
+++ DataProcessorTemplateFunction/DataProcessorTemplateFunction/DataProcessorTemplateFunction.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+#include <type_traits>
+#include <string>
+
+template <typename T>
+T functions() {
+
+	std::string s = "1T";
+	int i = 1;
+	bool b = false;
+
+	if (typeid(std::string) == typeid(T)) {
+		return s;
+	}
+	if (typeid(int) == typeid(T)) {
+		return i;
+	}
+	if (typeid(bool) == typeid(T)) {
+		return b;
+	}
+	return T();
+};
+
+int main() {
+	std::string a = functions<decltype(a)>();
+	std::cout << a;
+}
