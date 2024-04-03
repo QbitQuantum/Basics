6bd41eb59b897979d7e71cc9e3e685efe9d98f8e
DenisQbit <billchifer@bk.ru>
2024-04-03 14:44:58 +0700

    Реализация свёрточной функции
    :+1:

--- Convolution_test/Convolution_test/Convolution_test.cpp
+++ Convolution_test/Convolution_test/Convolution_test.cpp
@@ -79,46 +79,39 @@ void level_insert(T(&arr1)[Ta], D(&arr2)[Da]) {
     for (int i = 0; i < Ta; ++i) {
         for (int j = 0; j < Da; ++j) {
             db[i][j] = arr1[i] * arr2[j];
-            cout << db[i][j] << " ";
         }
-        cout << endl;
     }
-        cout << endl;
-        cout <<"4 13 28 27 18" << endl;
-        cout <<"18 27 28 13 4" << endl; 
-        cout << endl;
     const int size_db = extent<decltype(db), 0>::value;
     const int size_db_2 = extent<decltype(db), 1>::value;
     const int parametr_db_min = min(size_db, size_db_2);
     const int parametr_db_max = max(size_db, size_db_2);
     int sum_k[parametr_db_max]{};
     int sum_z[parametr_db_min]{};
-    //Высота
+
     for (int i = 0; i < size_db; i++)
     {
-            for (int j = 0; j < size_db_2; j++)
-            {
+            for (int j = size_db_2-1; j >=0; j--)
+            {   
                 for (int k = 0; k < parametr_db_max; k++)
                 {
-                    if (i + j == -k) {
-                        cout << db[i][j] << " ";
+                    if (i + j == k) { 
                         sum_k[k] += db[i][j];
                     }
                 }
                 for (int z = 1; z <= parametr_db_min; ++z)
                 {
-                    if (i + j == z) {
-                        sum_z[z - 1] += db[i][j];
+                    if (i + j == size_db + size_db_2 - z) {
+                        sum_z[z -1] += db[i][j];
                     }
                 }
             }
     }
-    reverse(sum_k, sum_k + parametr_db_max);
     for (int i = 0; i < parametr_db_max; i++) {
-        //std::cout << sum_k[i] << " "; // Выводим каждый элемент массива
+        std::cout << sum_k[i] << " ";
     }
-    for (int i = 0; i < parametr_db_min - 1; i++) {
-        //std::cout << sum_z[i] << " "; // Выводим каждый элемент массива
+    reverse(sum_z, sum_z + parametr_db_min);
+    for (int i = 0; i < parametr_db_min-1; i++) {
+        std::cout << sum_z[i] << " ";
     }
 }
 
