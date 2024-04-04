163412420cca372812a615ed752a7673923b3454
DenisQbit <billchifer@bk.ru>
2024-04-04 15:56:26 +0700

    Update Convolution_test.cpp
    1. Объединяем в одну функцию
    2. Правим имя функции
    3. Выводим в читабельном виде

--- Convolution_test/Convolution_test/Convolution_test.cpp
+++ Convolution_test/Convolution_test/Convolution_test.cpp
@@ -32,8 +32,8 @@ void level_2(T(&arr1)[Ta][Taa], int f, int f2) {
 }
 
 template <typename T, size_t Ta, typename D, size_t Da >
-void level_(T(&arr1)[Ta], D(&arr2)[Da]) {
-   int db[Ta][Da];
+void conv2D(T(&arr1)[Ta], D(&arr2)[Da], bool invert) {
+    int db[Ta][Da];
     for (int i = 0; i < Ta; ++i) {
         for (int j = 0; j < Da; ++j) {
             db[i][j] = arr1[i] * arr2[j];
@@ -45,54 +45,11 @@ void level_(T(&arr1)[Ta], D(&arr2)[Da]) {
     const int parametr_db_max = max(size_db, size_db_2);
     int sum_k[parametr_db_max]{};
     int sum_z[parametr_db_min]{};
-
-    for (int i = 0; i < size_db; i++)
-    {
-        for (int j = 0; j < size_db_2; j++)
-        {   
-            int difference_index = i - j;
-            for (int k = 0; k < parametr_db_max; k++)
+    if (invert) {
+        for (int i = 0; i < size_db; i++)
+        {
+            for (int j = size_db_2 - 1; j >= 0; j--)
             {
-                if (difference_index == -k) {
-                    sum_k[k] += db[i][j];
-                }
-            }
-            for (int z = 0; z <= parametr_db_min; z++)
-            {
-                if (difference_index == z) {
-                    sum_z[z - 1] += db[i][j];
-                }
-            }
-        }
-    }
-    reverse(sum_k, sum_k + parametr_db_max);
-    for (int i = 0; i < parametr_db_max; i++) {
-        std::cout << sum_k[i] << " "; // Выводим каждый элемент массива
-    }
-    for (int i = 0; i < parametr_db_min - 1; i++) {
-        std::cout << sum_z[i] << " "; // Выводим каждый элемент массива
-    }
-}
-
-template <typename T, size_t Ta, typename D, size_t Da >
-void level_insert(T(&arr1)[Ta], D(&arr2)[Da]) {
-   int db[Ta][Da];
-    for (int i = 0; i < Ta; ++i) {
-        for (int j = 0; j < Da; ++j) {
-            db[i][j] = arr1[i] * arr2[j];
-        }
-    }
-    const int size_db = extent<decltype(db), 0>::value;
-    const int size_db_2 = extent<decltype(db), 1>::value;
-    const int parametr_db_min = min(size_db, size_db_2);
-    const int parametr_db_max = max(size_db, size_db_2);
-    int sum_k[parametr_db_max]{};
-    int sum_z[parametr_db_min]{};
-
-    for (int i = 0; i < size_db; i++)
-    {
-            for (int j = size_db_2-1; j >=0; j--)
-            {   
                 int sum_index = i + j;
                 for (int k = 0; k < parametr_db_max; k++)
                 {
@@ -103,17 +60,47 @@ void level_insert(T(&arr1)[Ta], D(&arr2)[Da]) {
                 for (int z = 0; z <= parametr_db_min; z++)
                 {
                     if (sum_index == size_db + size_db_2 - z) {
-                        sum_z[z -1] += db[i][j];
+                        sum_z[z - 1] += db[i][j];
                     }
                 }
             }
+        }
+        for (int i = 0; i < parametr_db_max; i++) {
+            std::cout << sum_k[i] << " ";
+        }
+        reverse(sum_z, sum_z + parametr_db_min);
+        for (int i = 0; i < parametr_db_min - 1; i++) {
+            std::cout << sum_z[i] << " ";
+        }
     }
-    for (int i = 0; i < parametr_db_max; i++) {
-        std::cout << sum_k[i] << " ";
-    }
-    reverse(sum_z, sum_z + parametr_db_min);
-    for (int i = 0; i < parametr_db_min-1; i++) {
-        std::cout << sum_z[i] << " ";
+    else
+    {
+        for (int i = 0; i < size_db; i++)
+        {
+            for (int j = 0; j < size_db_2; j++)
+            {
+                int difference_index = i - j;
+                for (int k = 0; k < parametr_db_max; k++)
+                {
+                    if (difference_index == -k) {
+                        sum_k[k] += db[i][j];
+                    }
+                }
+                for (int z = 1; z <= parametr_db_min; ++z)
+                {
+                    if (difference_index == z) {
+                        sum_z[z - 1] += db[i][j];
+                    }
+                }
+            }
+        }
+        reverse(sum_k, sum_k + parametr_db_max);
+        for (int i = 0; i < parametr_db_max; i++) {
+            std::cout << sum_k[i] << " ";
+        }
+        for (int i = 0; i < parametr_db_min - 1; i++) {
+            std::cout << sum_z[i] << " ";
+        }
     }
 }
 
@@ -128,7 +115,10 @@ int main()
     int conv_test[] = { /*1, 2, 3,*/ 4, 5, 6,/* 7, 8, 9 */};
     int conv_couting[] = { /*1, 2, 3,4, 5, 6, */ 7, 8, 9, };
 
-    //level_(conv_1, conv_test);
-    level_insert(conv_1, conv_test);
+    std::cout << "conv2D = ";
+    conv2D(conv_1, conv_test, true);
+    std::cout << endl;
+    std::cout << "conv2D_invert = ";
+    conv2D(conv_1, conv_test, false);
 
 };
