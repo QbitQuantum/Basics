8d3b3fc902e384505a782f171be8250a57bfc4e9
DenisQbit <billchifer@bk.ru>
2024-04-08 16:42:01 +0700

    Update

--- code.cpp
+++ code.cpp
@@ -31,3 +31,336 @@ int main()
 
     return 0;
 }
+
+/*
+#include <iostream>
+#include <map>
+#include <ctime>
+
+// Предположим, что у нас есть класс BilixFlights с методом EnumerateActHeadNumber
+class BilixFlights {
+public:
+    int EnumerateActHeadNumber(int year) {
+        // Предположим, что этот метод возвращает некоторый результат на основе года
+        return year * 1000; // Примерный подсчет
+    }
+};
+
+// Предположим, что у нас есть класс Adding, который имеет Value и Element
+class Adding {
+public:
+    std::map<std::string, int> Value;
+    std::map<std::string, Element*> Element; // Предполагаем, что Element содержит указатели на Element
+};
+
+// Предположим, что у нас есть класс Element, который содержит метод DateTime
+class Element {
+public:
+    std::tm DateTime() {
+        std::time_t now = std::time(nullptr);
+        return *std::localtime(&now);
+    }
+};
+
+// Предположим, что YearOf извлекает год из даты
+int YearOf(const std::tm& date) {
+    return date.tm_year + 1900;
+}
+
+// Предположим, что _field возвращает строку, которая используется как ключ
+std::string _field(const std::string& key) {
+    return key;
+}
+
+int main() {
+    Adding __adding;
+    BilixFlights* flights = new BilixFlights(); // Создаем объект BilixFlights через указатель
+    Element* element = new Element(); // Создаем объект Element через указатель
+
+    // Предположим, что мы уже заполнили Element данными
+    __adding.Element[_field("date.begin")] = element;
+
+    // Выполняем операцию, соответствующую вашему шаблону
+    __adding.Value[_field("num")] = flights->EnumerateActHeadNumber(YearOf(__adding.Element[_field("date.begin")]->DateTime));
+
+    // Выводим результат
+    std::cout << "Value[num]: " << __adding.Value[_field("num")] << std::endl;
+
+    // Не забываем удалить объекты BilixFlights и Element
+    delete flights;
+    delete element;
+
+    return 0;
+}
+*/
+/*
+    // Предполагаем, что __adding.Element["date.begin"] содержит указатель на объект DateTime
+    std::time_t now = std::time(nullptr);
+    std::tm now_tm;
+    localtime_s(&now_tm, &now);
+    adding.Element["date.begin"] = new DateTime(&now_tm);
+*/
+
+/*
+#include <iostream>
+#include <map>
+#include <string>
+#include <ctime>
+
+class DateTime {
+public:
+    int Year() const {
+        // Возвращаем год из даты
+        return date_tm->tm_year + 1900; // tm_year is years since 1900
+    }
+
+    DateTime(std::tm* t) : date_tm(t) {}
+
+private:
+    std::tm* date_tm;
+};
+
+class BilixFlights {
+public:
+    int EnumerateActHeadNumber(int year) {
+        // Логика для извлечения года из даты
+        // Заглушка
+        return year;
+    }
+};
+
+struct __adding {
+    std::map<std::string, int> Value;
+    std::map<std::string, DateTime*> Element;
+};
+
+std::string _field(const std::string& key) {
+    return key;
+}
+
+int YearOf(DateTime* dateTime) {
+    return dateTime->Year();
+}
+
+int main() {
+    __adding adding;
+    BilixFlights* flights = new BilixFlights();
+
+    std::time_t now = std::time(nullptr);
+    std::tm now_tm;
+    localtime_s(&now_tm, &now); // Используем localtime_s вместо localtime
+
+    adding.Element[_field("date.begin")] = new DateTime(&now_tm);
+
+    int year = YearOf(adding.Element[_field("date.begin")]);
+    adding.Value[_field("num")] = flights->EnumerateActHeadNumber(year);
+
+    std::cout << "Value[\"num\"] = " << adding.Value[_field("num")] << std::endl;
+
+    delete adding.Element[_field("date.begin")];
+    delete flights;
+
+    return 0;
+}
+*/
+/*
+#include <iostream>
+#include <map>
+#include <string>
+#include <ctime>
+#include <vector>
+
+typedef int TDateTime;
+const TDateTime EMPTY_DATE = 0;
+
+// Базовая структура данных
+struct BaseDataElement {
+    std::string Generic;
+    TDateTime DateTime = EMPTY_DATE;
+    std::vector<std::string> Array;
+    void Clear() {
+        Generic.clear();
+        DateTime = EMPTY_DATE;
+        Array.clear();
+    }
+};
+
+class OctetBilixFlights {
+public:
+    int EnumerateActHeadNumber(int year) {
+        return year;
+    }
+};
+
+struct __adding {
+    std::map<std::string, int> Value;
+    std::map<std::string, BaseDataElement*> Element;
+};
+
+std::string _field(const std::string& key) {
+    return key;
+}
+
+int YearOf(TDateTime dateTime) {
+    return dateTime;
+}
+
+int main() {
+    __adding __adding;
+    OctetBilixFlights* BilixFlights = new OctetBilixFlights();
+
+    std::time_t now = std::time(nullptr);
+    std::tm now_tm;
+    localtime_s(&now_tm, &now);
+
+
+    __adding.Element[_field("date.begin")] = new BaseDataElement();
+    __adding.Element[_field("date.begin")]->DateTime = now_tm.tm_year + 1900;
+
+    __adding.Value[_field("num")] = BilixFlights->EnumerateActHeadNumber(YearOf(__adding.Element[_field("date.begin")]->DateTime));
+
+    delete __adding.Element[_field("date.begin")];
+    delete BilixFlights;
+
+    return 0;
+}
+*/
+
+/*
+#include <iostream>
+#include <map>
+#include <string>
+#include <ctime>
+#include <vector>
+
+typedef int TDateTime;
+const TDateTime EMPTY_DATE = 0;
+
+// Базовая структура данных
+struct BaseDataElement {
+    std::string Generic;
+    TDateTime DateTime = EMPTY_DATE;
+    std::vector<std::string> Array;
+    void Clear() {
+        Generic.clear();
+        DateTime = EMPTY_DATE;
+        Array.clear();
+    }
+};
+
+class OctetBilixFlights {
+public:
+    int EnumerateActHeadNumber(int year) {
+        return year;
+    }
+};
+
+struct __adding {
+    std::map<std::string, int> Value;
+    std::map<std::string, BaseDataElement*> Element;
+};
+
+std::string _field(const std::string& key) {
+    return key;
+}
+
+int YearOf(TDateTime dateTime) {
+    return dateTime;
+}
+
+int main() {
+    __adding __adding;
+    OctetBilixFlights* BilixFlights = new OctetBilixFlights();
+
+    std::time_t now = std::time(nullptr);
+    std::tm now_tm;
+    localtime_s(&now_tm, &now);
+
+
+    __adding.Element[_field("date.begin")] = new BaseDataElement();
+    __adding.Element[_field("date.begin")]->DateTime = now_tm.tm_year + 1900;
+
+    __adding.Value[_field("num")] = BilixFlights->EnumerateActHeadNumber(YearOf(__adding.Element[_field("date.begin")]->DateTime));
+
+    delete __adding.Element[_field("date.begin")];
+    delete BilixFlights;
+
+    return 0;
+}
+*/
+/*
+#include <iostream>
+#include <map>
+#include <string>
+#include <ctime>
+#include <vector>
+
+typedef int TDateTime;
+const TDateTime EMPTY_DATE = 0;
+
+// Базовая структура данных
+struct BaseDataElement {
+    std::string Generic;
+    TDateTime DateTime = EMPTY_DATE;
+    std::vector<std::string> Array;
+    void Clear() {
+        Generic.clear();
+        DateTime = EMPTY_DATE;
+        Array.clear();
+    }
+};
+
+class OctetBilixFlights {
+public:
+    int EnumerateActHeadNumber(int year) {
+        return year;
+    }
+};
+
+struct __adding {
+    std::map<std::string, int> Value;
+    std::map<std::string, BaseDataElement*> Element;
+};
+
+std::string _field(const std::string& key) {
+    return key;
+}
+
+int YearOf(TDateTime dateTime) {
+    return dateTime;
+}
+
+// Функция для инициализации элемента и установки даты
+void InitializeElementAndSetDate(__adding& adding, const std::string& key, TDateTime dateTime) {
+    adding.Element[_field(key)] = new BaseDataElement();
+    adding.Element[_field(key)]->DateTime = dateTime;
+}
+
+// Функция для удаления элемента
+void DeleteElement(__adding& adding, const std::string& key) {
+    delete adding.Element[_field(key)];
+    adding.Element.erase(_field(key));
+}
+
+int main() {
+    __adding __adding;
+    OctetBilixFlights* BilixFlights = new OctetBilixFlights();
+
+    std::time_t now = std::time(nullptr);
+    std::tm now_tm;
+    localtime_s(&now_tm, &now);
+
+    // Инициализация элемента и установка даты
+    InitializeElementAndSetDate(__adding, "date.begin", now_tm.tm_year + 1900);
+
+    // Использование элемента
+    __adding.Value[_field("num")] = BilixFlights->EnumerateActHeadNumber(YearOf(__adding.Element[_field("date.begin")]->DateTime));
+
+    // Удаление элемента
+    DeleteElement(__adding, "date.begin");
+
+    delete BilixFlights;
+
+    return 0;
+}
+*/
