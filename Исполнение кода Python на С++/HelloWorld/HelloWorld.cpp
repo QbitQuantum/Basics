7b42bdf4222a62f70851caac0e7e2dc949c23446
DenisQbit <billchifer@bk.ru>
2024-06-28 10:05:42 +0700

    Update C++

--- Исполнение кода Python на С++/HelloWorld/HelloWorld.cpp
+++ Исполнение кода Python на С++/HelloWorld/HelloWorld.cpp
@@ -14,6 +14,12 @@ import time
 def q(q,s):
     return time.time()
 
+def s(q,s):
+    return time.time()
+
+def t(q,s):
+    return time.time()
+
 )";
 
 	MyClass();
@@ -39,15 +45,14 @@ auto MyClass::Run()
     PyRun_SimpleStringFlags(this->program, NULL);
     auto name = "q";
     PyObject* T = PyTuple_New(2);
-    PyTuple_SetItem(T, 0, PyLong_FromLong(2));
-    PyTuple_SetItem(T, 1, PyLong_FromLong(3));
-    long result = CALL("q", T);
+    PyTuple_SetItem(PyTuple_New(2), 0, PyLong_FromLong(2));
+    PyTuple_SetItem(PyTuple_New(2), 1, PyLong_FromLong(3));
+    long result = CALL("s", PyTuple_New(2));
 
     std::cout << "Result: " << result << std::endl;
     Py_DECREF(T);
     
 }
-
 
 int main() {
     MyClass python;
